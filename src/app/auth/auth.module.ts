92e527945f36aaf611843e24694f88b5f1f62a6f
rauumartinez03 <raul.us03@gmail.com>
2024-05-04 18:19:47 +0200

    Creados componentes basicos de la app

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthLayoutComponent,
+    LoginPageComponent,
+    RegisterPageComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
---
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './login-page.component.html',
+  styles: ``
+})
+export class LoginPageComponent {
+
+}
